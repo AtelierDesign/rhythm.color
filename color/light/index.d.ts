9c1192e6759a56acb5301be66e13901838132101
chvndler <70621588+chvndler@users.noreply.github.com>
2022-05-31 10:49:21 -0400

    push

---
+++ color/light/index.d.ts
@@ -0,0 +1,6 @@
+export { lime } from './lime';
+export { limeA } from './limeA';
+export { mauve } from './mauve';
+export { mauveA } from './mauveA';
+export { rhythm } from './rhythm';
+export { rhythmA } from './rhythmA';
